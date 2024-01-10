c371e13a240176abb8331817a5cec4eab3159b72
Timo <unjky@student.kit.edu>
2024-01-10 15:08:19 +0000

    tag schemas

---
+++ app/schemas/tags/create_update_tag_body.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class TagCreateAndUpdateBody(BaseModel):
+    name: str
+    colour: str
+    exportable: bool
+    org_id: str
+    user_id: str
+    hide_tag: bool
+    numerical_value: str
+    is_galaxy: bool
+    is_custom_galaxy: bool
+    inherited: int
+
+    class Config:
+        orm_mode = True
