fe487112d9a85bf068241bfbdb8d438f21944ed9
unjky <unjky@student.kit.edu>
2024-01-10 22:13:47 +0100

    fix: operating system does not distinguish between 'feeds' and 'Feeds'

--- app/models/feed.py
+++ app/models/feed.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Boolean, Text
+from sqlalchemy import Column, String, Boolean, Integer
 from ..database import Base
 
 
@@ -21,14 +21,14 @@ class Feed(Base):
     event_id = Column(String)
     publish = Column(Boolean)
     override_ids = Column(Boolean)
-    settings = Column(Text)
+    settings = Column(String)
     input_source = Column(String)
     delete_local_file = Column(Boolean)
     lookup_visible = Column(Boolean)
-    headers = Column(Text)
+    headers = Column(String)
     caching_enabled = Column(Boolean)
     force_to_ids = Column(Boolean)
     orgc_id = Column(String)
     cache_timestamp = Column(String)
-    cached_elements = Column(Text)  # new
-    coverage_by_other_feeds = Column(Text)  # new
+    cached_elements = Column(Integer)  # new
+    coverage_by_other_feeds = Column(String)  # new
