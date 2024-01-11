adf97fea6140aa705d48e7e2eef297a5c3634541
AextraGit <wunderleaaron@gmail.com>
2024-01-11 23:01:56 +0100

    feat: added content to models/auth_key.py

---
+++ app/models/user_settings.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String, Boolean, Integer
+from sqlalchemy.orm import relationship
+from ..database import Base
+
+class UserSettings(Base):
+    id: Column(String)
+    setting: Column(String)
+    user_id: Column(String)
+    timestamp: Column(String)
+    value: relationship("UserSettingsPosition", backref="UserSettings")
+class UserSettingsValue(Base):
+    widget: Column(String)
+    position: relationship("UserSettingsPosition", backref="UserSettingsValue")
+class UserSettingsPosition(Base):
+    x: Column(String)
+    y: Column(String)
+    width: Column(String)
+    height: Column(String)
