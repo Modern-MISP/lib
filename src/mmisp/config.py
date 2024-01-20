021a0bdd090329abc250e1b478d1f373c774d560
Jakob Christoph Vogler <jakob@jcvogler.com>
2024-01-20 23:18:10 +0100

    feat: introduce environment managed config

---
+++ src/mmisp/config.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from os import getenv
+
+
+class ConfigType:
+    def __init__(
+        self,
+        DATABASE_URL: str,
+        HASH_SECRET: str,
+        DASHBOARD_URL: str | None = None,
+    ):
+        self.DATABASE_URL = DATABASE_URL
+        self.HASH_SECRET = HASH_SECRET
+        self.DASHBOARD_URL = DASHBOARD_URL
+
+
+load_dotenv(".env")
+
+
+config: ConfigType = ConfigType(
+    DATABASE_URL=getenv("DATABASE_URL") or "",
+    HASH_SECRET=getenv("HASH_SECRET") or "",
+    DASHBOARD_URL=getenv("DASHBOARD_URL"),
+)
