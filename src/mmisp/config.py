99e0e97a34e66d7d77e7ec2c674fc78ac1387e2b
Jakob Christoph Vogler <uzjcs@student.kit.edu>
2024-01-22 21:39:07 +0000

    feat: add authorize function

--- src/mmisp/config.py
+++ src/mmisp/config.py
@@ -8,10 +8,14 @@ class ConfigType:
         self,
         DATABASE_URL: str,
         HASH_SECRET: str,
+        WORKER_KEY: str,
+        WORKER_URL: str,
         DASHBOARD_URL: str | None = None,
     ):
         self.DATABASE_URL = DATABASE_URL
         self.HASH_SECRET = HASH_SECRET
+        self.WORKER_KEY = WORKER_KEY
+        self.WORKER_URL = WORKER_URL
         self.DASHBOARD_URL = DASHBOARD_URL
 
 
@@ -21,5 +25,7 @@ load_dotenv(".env")
 config: ConfigType = ConfigType(
     DATABASE_URL=getenv("DATABASE_URL") or "",
     HASH_SECRET=getenv("HASH_SECRET") or "",
+    WORKER_KEY=getenv("WORKER_KEY") or "",
+    WORKER_URL=getenv("WORKER_URL") or "",
     DASHBOARD_URL=getenv("DASHBOARD_URL"),
 )
