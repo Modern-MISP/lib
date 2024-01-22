c7da880c427f36639057e334b3b4efda9fcad993
Jakob Christoph Vogler <uzjcs@student.kit.edu>
2024-01-22 22:11:37 +0000

    chore: add database diff script

---
+++ src/mmisp/db/print_changes.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+"""
+USAGE:
+    - Create env file with the variables
+        MYSQL_USER
+        MYSQL_PASSWORD
+        MYSQL_HOST
+        MYSQL_DBNAME
+    - Populate your environment `export $(cat ENVFILE)`
+    - Run `python -m mmisp.db.print_changes`
+"""
+
+
+import os
+
+from alembic.migration import MigrationContext
+from alembic.autogenerate import compare_metadata
+from sqlalchemy import create_engine, MetaData
+import pprint
+
+# import all models, so Base is populated
+# the not imported modules have thrown errors
+from .models import (  # noqa: F401
+    #    attribute,
+    #    auth_key,
+    event,
+    feed,
+    galaxy,
+    noticelist,
+    object,
+    organisation,
+    role,
+    server,
+    sharing_group,
+    sighting,
+    tag,
+    #    taxonomy,
+    user,
+    user_settings,
+    warninglist,
+)
+from .database import Base
+
+
+myuser = os.getenv("MYSQL_USER")
+password = os.getenv("MYSQL_PASSWORD")
+host = os.getenv("MYSQL_HOST")
+db_name = os.getenv("MYSQL_DBNAME")
+
+engine = create_engine(f"mysql+mysqlconnector://{myuser}:{password}@{host}/{db_name}")
+
+
+metadata = MetaData()
+
+mc = MigrationContext.configure(engine.connect())
+
+diff = compare_metadata(mc, Base.metadata)
+pprint.pprint(diff, indent=2, width=20)
