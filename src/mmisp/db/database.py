e12e7c78d6df9b3ef6d624c7aa8012a7ad04b4b9
unjky <unjky@student.kit.edu>
2024-01-26 16:43:21 +0100

    fix: tables are only created if the db is accessible

--- src/mmisp/db/database.py
+++ src/mmisp/db/database.py
@@ -2,6 +2,7 @@ from typing import Iterator
 
 from sqlalchemy import create_engine
 from sqlalchemy.engine.url import make_url
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session, sessionmaker
 
@@ -11,9 +12,15 @@ url = make_url(config.DATABASE_URL).set(drivername="mysql+mysqlconnector")
 engine = create_engine(url)
 
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
 Base = declarative_base()
+
+
+def create_tables() -> None:
+    try:
+        with engine.connect():
+            Base.metadata.create_all(bind=engine)
+    except OperationalError:
+        pass
 
 
 def get_db() -> Iterator[Session]:
