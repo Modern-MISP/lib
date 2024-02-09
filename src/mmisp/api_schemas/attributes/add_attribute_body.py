8f60f2d2bd4c159cd6dc989a709ed617c1444436
Timo <unjky@student.kit.edu>
2024-02-09 11:10:32 +0100

    refactor: 'root_validator'

--- src/mmisp/api_schemas/attributes/add_attribute_body.py
+++ src/mmisp/api_schemas/attributes/add_attribute_body.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Optional
+from typing import Any, Optional
 
 from pydantic import BaseModel, root_validator
 
@@ -25,8 +25,8 @@ class AddAttributeBody(BaseModel):
     event_uuid: str | None = None
 
     @root_validator
-    def ensure_value_or_value1_is_set(cls, data: Dict[str, Any]) -> None:  # noqa: ANN101
-        required_values: list[str] = [data.get("value"), data.get("value1")]
+    def ensure_value_or_value1_is_set(cls, data: dict[str, Any]) -> Optional[dict[str, Any]]:  # noqa: ANN101
+        required_values: list[str] = [str(data.get("value")), str(data.get("value1"))]
         if all(item is None for item in required_values):
             raise ValueError("value or value1 has to be set")
         return data
