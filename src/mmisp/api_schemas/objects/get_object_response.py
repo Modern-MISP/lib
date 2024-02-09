37db0d57abbf8ee19f952c61c1652560aa428115
Timo <unjky@student.kit.edu>
2024-02-09 21:26:08 +0000

    upg/objects

--- src/mmisp/api_schemas/objects/get_object_response.py
+++ src/mmisp/api_schemas/objects/get_object_response.py
@@ -1,31 +1,51 @@
-from pydantic import BaseModel
+from typing import Any, Dict, Optional
+
+from pydantic import BaseModel, validator
 
 from mmisp.api_schemas.attributes.get_all_attributes_response import GetAllAttributesResponse
 from mmisp.api_schemas.events.get_event_response import ObjectEventResponse
 
 
-class ObjectWithAttributesAndEventResponse(BaseModel):
+class ObjectWithAttributesResponse(BaseModel):
     id: str
+    uuid: str
     name: str
-    description: str
-    meta_category: str
-    event_id: str
-    distribution: str
+    meta_category: str | None = None
+    description: str | None = None
+    template_id: str | None = None
     template_uuid: str | None = None
-    template_version: str | None = None
-    uuid: str | None = None
-    timestamp: str | None = None
+    template_version: str
+    event_id: str
+    timestamp: str
+    distribution: str
     sharing_group_id: str | None = None
-    comment: str | None = None
-    deleted: bool | None = None
+    comment: str
+    deleted: bool
     first_seen: str | None = None
     last_seen: str | None = None
-    attributes: list[GetAllAttributesResponse] | None = None
+    attributes: list[GetAllAttributesResponse]
     event: ObjectEventResponse | None = None
 
+    @validator("sharing_group_id", always=True)
+    def check_sharing_group_id(cls, value: Any, values: Dict[str, Any]) -> Optional[int]:  # noqa: ANN101
+        """
+        If distribution equals 4, sharing_group_id will be shown.
+        """
+        distribution = values.get("distribution", None)
+        if distribution == "4" and value is not None:
+            return value
+        return None
 
-class ObjectViewResponse(BaseModel):
-    object: ObjectWithAttributesAndEventResponse
+
+class ObjectResponse(BaseModel):
+    object: ObjectWithAttributesResponse
+
+    class Config:
+        orm_mode = True
+
+
+class ObjectSearchResponse(BaseModel):
+    response: list[ObjectResponse]
 
     class Config:
         orm_mode = True
