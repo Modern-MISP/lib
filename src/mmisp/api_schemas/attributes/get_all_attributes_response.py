9692feeb95f1e1b74b8b539702e32c1adbbb3602
Timo <unjky@student.kit.edu>
2024-02-11 22:38:49 +0100

    feat: add '@validator' decorators to convert specific fields to strings

--- src/mmisp/api_schemas/attributes/get_all_attributes_response.py
+++ src/mmisp/api_schemas/attributes/get_all_attributes_response.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from typing import Any, Dict, Optional
+
+from pydantic import BaseModel, validator
 
 
 class GetAllAttributesResponse(BaseModel):
@@ -21,6 +23,30 @@ class GetAllAttributesResponse(BaseModel):
     disable_correlation: bool | None = None
     first_seen: str | None = None
     last_seen: str | None = None
+
+    @validator("sharing_group_id", always=True)
+    def check_sharing_group_id(cls, value: Any, values: Dict[str, Any]) -> Optional[int]:  # noqa: ANN101
+        """
+        If distribution equals 4, sharing_group_id will be shown.
+        """
+        distribution = values.get("distribution", None)
+        if distribution == "4" and value is not None:
+            return value
+        return None
+
+    @validator(
+        "event_id",
+        "object_id",
+        "timestamp",
+        "distribution",
+        "sharing_group_id",
+        "first_seen",
+        "last_seen",
+        pre=True,
+        allow_reuse=True,
+    )
+    def convert_to_string(cls, value: Optional[str]) -> Optional[str]:  # noqa: ANN101
+        return str(value) if value is not None else None
 
     class Config:
         orm_mode = True
