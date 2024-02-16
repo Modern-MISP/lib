e09b18b521bf221a978590933770e0581359e6f6
Jonathan Andreas Full <uvmbf@student.kit.edu>
2024-02-16 17:03:31 +0000

    Resolve MA-22 "Warninglists"

--- src/mmisp/api_schemas/warninglists/create_warninglist_body.py
+++ src/mmisp/api_schemas/warninglists/create_warninglist_body.py
@@ -2,13 +2,13 @@ from pydantic import BaseModel, Field
 
 
 class CreateWarninglistBody(BaseModel):
-    name: str = Field(max_length=255)
+    name: str = Field(min_length=1, max_length=255)
     type: str
-    description: str = Field(max_length=65535)
+    description: str = Field(min_length=1, max_length=65535)
     default: bool
     category: str
     valid_attributes: str
-    values: str = Field(max_length=65535)
+    values: str = Field(min_length=1, max_length=65535)
 
     class Config:
         orm_mode = True
