d1c67b156cd266bba981fc135d071e6d1e6a9f04
Jonathan Andreas Full <uvmbf@student.kit.edu>
2024-03-13 17:16:30 +0000

    Refactor warninglists

--- src/mmisp/api_schemas/warninglists/get_selected_all_warninglists_response.py
+++ src/mmisp/api_schemas/warninglists/get_selected_all_warninglists_response.py
@@ -3,8 +3,12 @@ from pydantic import BaseModel
 from mmisp.api_schemas.warninglists.warninglist_response import WarninglistAttributes
 
 
+class WarninglistsResponse(BaseModel):
+    Warninglist: WarninglistAttributes
+
+
 class GetSelectedAllWarninglistsResponse(BaseModel):
-    response: list[WarninglistAttributes]
+    response: list[WarninglistsResponse]
 
     class Config:
         orm_mode = True
