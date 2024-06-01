842c6d00f82745e59f5a5476db222dadccbcbe65
Jan Philipp Schlageter <uzslw@student.kit.edu>
2024-06-01 18:22:29 +0000

    Update file add_user_edit_user.py

--- src/mmisp/api_schemas/users.py
+++ src/mmisp/api_schemas/users.py
@@ -50,3 +50,32 @@ class UsersViewMeResponse(BaseModel):
     Role: Role
     UserSetting: list = []
     Organisation: Organisation
+
+class UserAttributesResponse(BaseModel):
+    id: str
+    org_id: str | None = None
+    email: str | None = None
+    autoalert: bool
+    invited_by: str | None = None
+    gpgkey: str | None = None
+    certif_public: str | None = None
+    termsaccepted: bool | None = None
+    role_id: str | None = None
+    change_pw: bool | None = None
+    contactalert: bool
+    disabled: bool
+    expiration: str | None = None
+    current_login: str | None = None
+    last_login: str | None = None
+    force_logout: bool
+    date_created:str
+    date_modified: str | None = None
+    external_auth_required: bool
+    external_auth_key: str | None = None
+    last_api_access: str | None = None
+    notification_daily: bool
+    notification_weekly: bool
+    notification_monthly: bool
+    totp: str | None = None
+    hotp_counter: str | None = None
+    last_pw_change: str | None = None
