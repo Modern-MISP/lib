d9092b93641ae0425379bb2d4420ccbe0bc44744
ulpov <ulpov@student.kit.edu>
2024-06-20 18:10:48 +0200

    formatting

--- src/mmisp/api_schemas/responses/check_graph_response.py
+++ src/mmisp/api_schemas/responses/check_graph_response.py
@@ -7,11 +7,13 @@ class IsAcyclicInfo():
     
 class IsAcyclic():
     """
-    - **is_acyclic** False if the graph contains at least one cycle.
-    - **cycles** A list with entries containing 2 node id's and a "Cycle" string.
-     
-    It should look like this:
-    '''
+    Represents the acyclic status of a graph and details of detected cycles.
+
+    - **is_acyclic**: False if the graph contains at least one cycle.
+    - **cycles**: A list of entries, each containing two node IDs and a "Cycle" string.
+
+    Example:
+    ```json
     "is_acyclic": {
         "is_acyclic": false,
         "cycles": [
@@ -27,7 +29,7 @@ class IsAcyclic():
             ]
         ]
     }
-    '''
+    ```
     """
     is_acyclic: bool
     cycles: List[IsAcyclicInfo]
@@ -37,9 +39,8 @@ class MultibleOutputConnection():
     """
     Represents the acyclic status of a graph and details of detected cycles.
 
-    Attributes:
-    - **is_acyclic (bool)**: False if the graph contains at least one cycle.
-    - **cycles (List[IsAcyclicInfo])**: A list of entries, each containing two node IDs and a "Cycle" string.
+    - **is_acyclic**: False if the graph contains at least one cycle.
+    - **cycles**: A list of entries, each containing two node IDs and a "Cycle" string.
 
     Example:
     ```json
@@ -77,7 +78,6 @@ class PathWarnings():
     """
     Represents warnings for paths in a graph.
 
-    Attributes:
     - **has_path_warnings**: True if the graph contains at least one warning.
     - **edges**: A list containing all connections which are flagged as warnings.
 
@@ -87,8 +87,8 @@ class PathWarnings():
         "has_path_warnings": true,
         "edges": [
             [
-                "5",
-                "2",
+                5,
+                2,
                 "This path leads to a blocking node from a non-blocking context",
                 true,
                 "stop-execution",
@@ -104,11 +104,37 @@ class PathWarnings():
 class CheckGraphResponse():
     """
     Response schema from the API for checking a graph.
-    
-    - **is_acyclic** Indicates whether the graph is acyclig and gives information for the first cycle.
-    - **multiple_output_connection** Indicates whether the graph has multiple output connections. 
-    States what modules have multiple output connections
-    - **path_warnings** If a path leads to a blocking node from a 'Concurrent Task' node, a warning is recorded. 
+
+    Attributes:
+    - **is_acyclic**: Indicates whether the graph is acyclic and provides information about the first detected cycle, if any.
+    - **multiple_output_connection**: Indicates whether the graph has illegal multiple output connections, 
+    detailing the nodes involved.
+    - **path_warnings**: Records warnings if a path leads to a blocking node from a 'Concurrent Task' node, providing relevant details.
+
+    Example JSON structure:
+    ```json
+    {
+        "is_acyclic": {
+            "is_acyclic": false,
+            "cycles": [
+                [4, 3, "Cycle"],
+                [3, 4, "Cycle"]
+            ]
+        },
+        "multiple_output_connection": {
+            "has_multiple_output_connection": true,
+            "edges": {
+                "1": [5, 3]
+            }
+        },
+        "path_warnings": {
+            "has_path_warnings": true,
+            "edges": [
+                [5, 2, "This path leads to a blocking node from a non-blocking context", true, "stop-execution", 2]
+            ]
+        }
+    }
+    ```
     """
     is_acyclic: IsAcyclic
     multiple_output_connection: MultibleOutputConnection
