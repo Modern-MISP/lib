130e2350730be8f86c6485b3689d2d8a16904d8a
Maximilian Bosch <maximilian@mbosch.me>
2024-07-02 12:30:57 +0200

    Logger for workflows

--- src/mmisp/db/models/log.py
+++ src/mmisp/db/models/log.py
@@ -21,7 +21,7 @@ class Log(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
     title: Mapped[str] = mapped_column(Text, nullable=True)
-    created: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    created: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.utcnow)
     model: Mapped[str] = mapped_column(String(80), nullable=False)
     model_id: Mapped[int] = mapped_column(Integer, nullable=False)
     action: Mapped[str] = mapped_column(String(20), nullable=False)
@@ -30,4 +30,4 @@ class Log(Base):
     email: Mapped[str] = mapped_column(String(255), nullable=False)
     org: Mapped[str] = mapped_column(String(255), nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=True)
-    ip: Mapped[str] = mapped_column(String(45), nullable=False)
+    ip: Mapped[str] = mapped_column(String(45), nullable=True)
---
+++ tests/lib/test_logging.py
@@ -0,0 +1,88 @@
+import uuid
+from unittest.mock import Mock
+
+import pytest
+
+from mmisp.db.models.workflow import Workflow
+from mmisp.lib.logging import ApplicationLogger
+from mmisp.workflows.graph import Apperance, Trigger, WorkflowGraph
+from mmisp.workflows.modules import Overhead
+
+
+def test_no_log(workflow: Workflow) -> None:
+    workflow.debug_enabled = False
+
+    mock = Mock()
+    logger = ApplicationLogger(mock)
+
+    log_entry = logger.log_workflow_debug_message(workflow, "Something happened")
+
+    assert log_entry is None
+    assert mock.add.called == 0
+
+
+def test_log(workflow: Workflow) -> None:
+    mock = Mock()
+    logger = ApplicationLogger(mock)
+
+    log_entry = logger.log_workflow_debug_message(workflow, "Something happened")
+
+    assert mock.add.called == 1
+    mock.add.assert_called_with(log_entry)
+
+    assert log_entry.model_id == 23
+    assert log_entry.action == "execute_workflow"
+    assert log_entry.user_id == "SYSTEM"
+    assert log_entry.title == "Something happened"
+
+
+def test_error_log(workflow: Workflow) -> None:
+    # Make sure that this gets written regardless of
+    # whether or not debugging is enabled.
+    for debug in [True, False]:
+        workflow.debug_enabled = debug
+        mock = Mock()
+        logger = ApplicationLogger(mock)
+
+        log_entry = logger.log_workflow_execution_error(workflow, "Something happened")
+
+        assert mock.add.called == 1
+        mock.add.assert_called_with(log_entry)
+
+        assert log_entry.model_id == 23
+        assert log_entry.action == "execute_workflow"
+
+
+@pytest.fixture
+def workflow(trigger: Trigger) -> Workflow:
+    return Workflow(
+        id=23,
+        uuid=str(uuid.uuid4()),
+        name="Demo workflow",
+        description="",
+        timestamp=0,
+        enabled=True,
+        trigger_id="demo",
+        debug_enabled=True,
+        data=WorkflowGraph(
+            nodes=[trigger],
+            root=trigger,
+            frames=[],
+        ),
+    )
+
+
+@pytest.fixture
+def trigger() -> Trigger:
+    return Trigger(
+        name="demo",
+        scope="local",
+        description="Hello World",
+        expect_misp_core_format=False,
+        blocking=False,
+        overhead=Overhead.LOW,
+        raw_data={},
+        inputs={},
+        outputs={},
+        apperance=Apperance((0, 0), False, "", None),
+    )
