9cb27d32ae7ca7e558cde05c0317d026739e1c5e
Gabriel <gabriel.heilinger1@gmail.com>
2024-07-05 01:15:49 +0200

    lint

--- src/mmisp/workflows/input.py
+++ src/mmisp/workflows/input.py
@@ -311,7 +311,7 @@ class WorkflowInput:
     """
 
     filters: List[Filter]
-    filtered_data: List[RoamingData]
+    filtered_data: List[RoamingData] | List[List[RoamingData]]
 
     def __init__(self: Self, data: RoamingData, user: "User", workflow: "Workflow") -> None:
         self.__unfiltered_data = data
