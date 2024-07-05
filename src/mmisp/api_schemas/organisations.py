500891ca4c2f791e4000bda82b8765b4adf0486c
Thomas Lennart Seibold <unhxm@student.kit.edu>
2024-07-05 12:45:11 +0000

    add response model

--- src/mmisp/api_schemas/organisations.py
+++ src/mmisp/api_schemas/organisations.py
@@ -22,3 +22,19 @@ class Organisation(BaseModel):
 
     class Config:
         orm_mode = True
+
+class GetOrganisationResponse(BaseModel):
+    id: str
+    name: str
+    date_created: datetime
+    date_modified: datetime
+    description: str | None = None
+    type: str
+    nationality: str | None = None
+    sector: str | None = None
+    created_by: str
+    uuid: str
+    contacts: str | None = None
+    local: bool
+    restricted_to_domain: str | None = None
+    landingpage: str | None = None
