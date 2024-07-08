3f0bb1f08aa29df9240eddc38418d6459922bee9
uvvjz@student.kit.edu <uvvjz@student.kit.edu>
2024-07-08 23:00:26 +0200

    feat: add/delete server

--- src/mmisp/api_schemas/servers.py
+++ src/mmisp/api_schemas/servers.py
@@ -1,17 +1,34 @@
 from pydantic import BaseModel
 
+
 class AddServer(BaseModel):
-    serverId: str
-    all_orgs: bool
+    name: str
+    url: str
+    priority: int
+    authkey: str
+    org_id: int
+    remote_org_id: int
+    internal: bool
+    push: bool
+    pull: bool
+    pull_rules: str
+    push_rules: str
+    push_galaxy_clusters: bool
+    caching_enabled: bool
+    unpublish_event: bool
+    publish_without_email: bool
+    self_signed: bool
+    skip_proxy: bool
+
+
+class AddServerResponse(BaseModel):
+    id: str
 
 class RemoveServer (BaseModel):
     id: str
     sharing_group_id: str
     server_id: str
     all_orgs: bool
-
-class ServersGetVersion(BaseModel):
-    pass
 
 class GetRemoteServersResponse(BaseModel):
     id: int
@@ -32,3 +49,8 @@ class GetRemoteServersResponse(BaseModel):
     skip_proxy: bool | None = None
     caching_enabled: bool | None = None
     priority: int | None = None
+
+
+class ServersGetVersion(BaseModel):
+    pass
+
