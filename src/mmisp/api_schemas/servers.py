1e989283b74e99da1a012a23f2bb66936392e9d5
khins <kath.hinschuetz@gmail.com>
2024-07-08 15:02:19 +0200

    feat: servers get all and get one

--- src/mmisp/api_schemas/servers.py
+++ src/mmisp/api_schemas/servers.py
@@ -13,3 +13,22 @@ class RemoveServer (BaseModel):
 class ServersGetVersion(BaseModel):
     pass
 
+class GetRemoteServersResponse(BaseModel):
+    id: int
+    name: str
+    url: str
+    authkey: str
+    org_id: int | None = None
+    push: bool
+    pull: bool
+    push_sightings: bool | None = None
+    push_galaxy_clusters: bool | None = None
+    pull_galaxy_clusters: bool | None = None
+    remote_org_id: int
+    publish_without_email: bool | None = None
+    unpublish_event: bool | None = None
+    self_signed: bool
+    internal: bool | None = None
+    skip_proxy: bool | None = None
+    caching_enabled: bool | None = None
+    priority: int | None = None
