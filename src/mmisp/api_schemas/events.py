a47e0bfe968ec31410d763c038fbbbc7d1e11622
uzslw <j-schlageter@gmx.de>
2024-07-13 17:09:41 +0200

    fix: wtf did my vscode

--- src/mmisp/api_schemas/events.py
+++ src/mmisp/api_schemas/events.py
@@ -22,6 +22,49 @@ class AddEditGetEventGalaxyClusterMeta(BaseModel):
     refs: list[str] | None = None
     kill_chain: str | None = None
 
+class FreeTextImportWorkerData(BaseModel):
+    data: str
+
+
+class FreeTextImportWorkerUser(BaseModel):
+    user_id: int
+
+
+class FreeTextImportWorkerBody(BaseModel):
+    user: FreeTextImportWorkerUser
+    data: FreeTextImportWorkerData
+
+    class Config:
+        orm_mode = True
+
+
+
+
+class AddAttributeViaFreeTextImportEventResponse(BaseModel):
+    comment: str | None = None
+    value: str
+    original_value: str
+    to_ids: str
+    type: str
+    category: str
+    distribution: str
+
+    class Config:
+        orm_mode = True
+
+
+class AddAttributeViaFreeTextImportEventAttributes(BaseModel):
+    value: str
+
+
+class AddAttributeViaFreeTextImportEventBody(BaseModel):
+    Attribute: AddAttributeViaFreeTextImportEventAttributes
+
+    class Config:
+        orm_mode = True
+
+
+
 
 class GetAllEventsGalaxyCluster(BaseModel):
     id: str
