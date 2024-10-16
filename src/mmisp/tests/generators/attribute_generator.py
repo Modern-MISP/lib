904dab17892d4865eb53ec2c8f7fefd11be684e6
Amadeus Haessler <mail@amadeushaessler.de>
2024-10-16 16:26:41 +0200

    Fixed enrichment tests

--- src/mmisp/tests/generators/attribute_generator.py
+++ src/mmisp/tests/generators/attribute_generator.py
@@ -2,6 +2,7 @@ import random
 
 from mmisp.api_schemas.attributes import GetAttributeAttributes
 from mmisp.lib.uuid import uuid
+from mmisp.plugins.models.attribute import AttributeTagWithRelationshipType, AttributeWithTagRelationship
 from mmisp.tests.generators.feed_generator import generate_number_as_str
 from mmisp.tests.generators.object_generator import generate_ids_as_str, generate_random_date_str, generate_random_str
 from mmisp.tests.generators.tag_generator import generate_get_attribute_tag_response
@@ -29,4 +30,35 @@ def generate_get_attribute_attributes_response() -> GetAttributeAttributes:
         event_uuid=uuid(),
         data=generate_random_str(),
         Tag=[generate_get_attribute_tag_response()]
+    )
+
+
+def generate_attribute_with_tag_relationship() -> AttributeWithTagRelationship:
+    attribute: GetAttributeAttributes = generate_get_attribute_attributes_response()
+    tags: list[AttributeTagWithRelationshipType] = []
+    for tag in attribute.Tag or []:
+        tags.append(AttributeTagWithRelationshipType(**tag.dict(), relationship_local=bool(random.getrandbits),
+                                                     relationship_type=generate_random_str()))
+
+    return AttributeWithTagRelationship(
+        id=attribute.id,
+        event_id=attribute.event_id,
+        object_id=attribute.object_id,
+        object_relation=attribute.object_relation,
+        category=attribute.category,
+        type=attribute.type,
+        value=attribute.value,
+        to_ids=attribute.to_ids,
+        uuid=attribute.uuid,
+        timestamp=attribute.timestamp,
+        distribution=attribute.distribution,
+        sharing_group_id=attribute.sharing_group_id,
+        comment=attribute.comment,
+        deleted=attribute.deleted,
+        disable_correlation=attribute.disable_correlation,
+        first_seen=attribute.first_seen,
+        last_seen=attribute.last_seen,
+        event_uuid=attribute.event_uuid,
+        data=attribute.data,
+        Tag=tags,
     )
