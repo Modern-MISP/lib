2986f3c1ed011162cc84e4ddee977be9e68a4fe5
Amadeus Haessler <mail@amadeushaessler.de>
2024-10-26 18:23:57 +0200

    Fixed correlation tests

--- src/mmisp/tests/generators/model_generators/attribute_generator.py
+++ src/mmisp/tests/generators/model_generators/attribute_generator.py
@@ -1,5 +1,10 @@
 from mmisp.db.models.attribute import Attribute
+from mmisp.lib.attributes import AttributeCategories
 
 
 def generate_attribute(event_id) -> Attribute:
     return Attribute(value="1.2.3.4", value1="1.2.3.4", type="ip-src", category="Network activity", event_id=event_id)
+
+
+def generate_text_attribute(event_id, value: str) -> Attribute:
+    return Attribute(value=value, type="text", category=AttributeCategories.OTHER.value, event_id=event_id)
