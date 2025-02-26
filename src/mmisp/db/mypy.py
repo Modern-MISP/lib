2dd1312c3c5b82767fd3bc873f078772229b9bcd
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2025-02-26 13:04:01 +0100

    use sqlalchemy v2 imports

--- src/mmisp/db/mypy.py
+++ src/mmisp/db/mypy.py
@@ -1,6 +1,11 @@
+import importlib.metadata
 from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
+from packaging.version import Version
+
+sa_version = Version(importlib.metadata.version("sqlalchemy"))
+
+if TYPE_CHECKING or sa_version >= Version("2.0"):
     from sqlalchemy.orm import Mapped, mapped_column  # type:ignore[attr-defined]
 else:
     from sqlalchemy import Column
