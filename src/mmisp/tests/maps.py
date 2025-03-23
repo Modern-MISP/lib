1d94ba565a27284a49847260cd31ccee6f26598d
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2025-03-23 22:33:52 +0100

    add pytest parametrize map

---
+++ src/mmisp/tests/maps.py
@@ -0,0 +1,7 @@
+access_test_objects_user_event_access_expect_granted = [
+    ("site_admin_user", "event_org1_1_unpublished"),
+    ("user_org1_publisher", "event_org1_1_unpublished"),
+]
+access_test_objects_user_event_access_expect_denied = [
+    ("user_org1_publisher", "event_org2_1_unpublished"),
+]
