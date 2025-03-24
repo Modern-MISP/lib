10c34d4a52d11282e60434c6d553b5e4d18b4f2c
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2025-03-24 13:14:33 +0100

    add cache for hash_secret, for faster testing

--- src/mmisp/util/crypto.py
+++ src/mmisp/util/crypto.py
@@ -9,6 +9,7 @@ def verify_secret(secret: str, secret_hash: str) -> bool:
     return context.verify(secret, secret_hash)
 
 
+@lru_cache(maxsize=32)  # normally not needed, but will increase testing speed
 def hash_secret(secret: str) -> str:
     context = CryptContext(schemes=["bcrypt", "argon2"], bcrypt__ident="2y")
     return context.hash(secret)
