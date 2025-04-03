db6083bbbbef7b86bbcfbbcfa2591f057af089cb
Amadeus Haessler <mail@amadeushaessler.de>
2025-04-03 21:20:32 +0200

    Fixed event generator

--- src/mmisp/tests/generators/event_generator.py
+++ src/mmisp/tests/generators/event_generator.py
@@ -7,10 +7,10 @@ from mmisp.lib import uuid
 from mmisp.tests.generators.feed_generator import random_string
 
 
-def generate_valid_random_create_event_data() -> AddEditGetEventDetails:
+def generate_valid_random_create_event_data(orgc_id: int, org_id: int) -> AddEditGetEventDetails:
     return AddEditGetEventDetails(
-        orgc_id=1,
-        org_id=1,
+        orgc_id=orgc_id,
+        org_id=org_id,
         date=datetime.date(year=random.randint(2000, 2024), month=random.randint(1, 12),
                            day=random.randint(1, 28)).strftime("%Y-%m-%d"),
         threat_level_id=random.randint(1, 4),
@@ -25,4 +25,5 @@ def generate_valid_random_create_event_data() -> AddEditGetEventDetails:
         sharing_group_id=1,
         disable_correlation=False,
         event_creator_email=f"generated-user+{time_ns()}@test.com",
+        protected=False,
     )
