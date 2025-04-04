4b075639ee9800a40d2f14e1926d70c1f86de65e
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2025-04-04 11:54:30 +0200

    use httpx instead of aiohttp

--- src/mmisp/util/async_download.py
+++ src/mmisp/util/async_download.py
@@ -1,39 +1,9 @@
 import asyncio
-from typing import Optional, Self
 
-from aiohttp import ClientSession
-from yarl import URL
+from httpx import AsyncClient, Response
 
 
-class Response:
-    """Represents the response from an HTTP request.
-
-    Attributes:
-        status: The HTTP status code of the response.
-        reason: The reason phrase associated with the status code.
-        data: The response body as a string.
-        url: The URL of the request.
-        ok: A boolean indicating whether the request was successful (status code 2xx).
-    """
-
-    def __init__(self: Self, status: int, reason: Optional[str], data: str, url: URL, ok: bool) -> None:
-        """Initializes a Response object.
-
-        Args:
-            status: The HTTP status code.
-            reason: The reason phrase for the status code.
-            data: The response body.
-            url: The URL of the request.
-            ok: Whether the request was successful.
-        """
-        self.status = status
-        self.reason = reason
-        self.data = data
-        self.url = url
-        self.ok = ok
-
-
-async def download_files(session: ClientSession, urls: list[str]) -> list[Response]:
+async def download_files(session: AsyncClient, urls: list[str]) -> list[Response]:
     """Downloads multiple files asynchronously.
 
     Args:
@@ -51,7 +21,7 @@ async def download_files(session: ClientSession, urls: list[str]) -> list[Respon
     return await asyncio.gather(*tasks)
 
 
-async def download_file(session: ClientSession, url: str) -> Response:
+async def download_file(session: AsyncClient, url: str) -> Response:
     """Downloads a single file asynchronously.
 
     Args:
@@ -61,6 +31,4 @@ async def download_file(session: ClientSession, url: str) -> Response:
     Returns:
         Response: The response object containing the result of the request.
     """
-    async with session.get(url) as response:
-        result = Response(response.status, response.reason, await response.text(), response.url, response.ok)
-        return result
+    return await session.get(url)
