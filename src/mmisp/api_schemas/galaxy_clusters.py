fcd772a0f39f9b9a36612f7a5717de106e78b41b
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2025-04-26 13:56:49 +0200

    adapt galaxy cluster schema

--- src/mmisp/api_schemas/galaxy_clusters.py
+++ src/mmisp/api_schemas/galaxy_clusters.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from uuid import UUID
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 from mmisp.api_schemas.events import AddEditGetEventGalaxyClusterRelation, GetAllEventsGalaxyClusterGalaxy
-from mmisp.api_schemas.galaxy_common import GetAllSearchGalaxiesAttributes
+from mmisp.api_schemas.galaxy_common import CommonGalaxyCluster, GetAllSearchGalaxiesAttributes
 from mmisp.api_schemas.organisations import GetOrganisationResponse, Organisation
 from mmisp.lib.distribution import DistributionLevels
 
@@ -16,30 +16,14 @@ class ExportGalaxyGalaxyElement(BaseModel):
     value: str
 
 
-class GetGalaxyClusterResponse(BaseModel):
-    id: int | None = None
-    uuid: UUID | None = None
-    collection_uuid: str
-    type: str
-    value: str
-    tag_name: str
-    description: str
-    galaxy_id: int
-    source: str
-    authors: list[str]
-    version: int
-    distribution: str
-    sharing_group_id: int | None = None
-    org_id: int
-    orgc_id: int
-    default: bool
-    locked: bool
-    extends_uuid: str | None = None
-    extends_version: int | None = None
-    published: bool
-    deleted: bool
-    Galaxy: GetAllSearchGalaxiesAttributes | None = None
+class GetGalaxyClusterResponse(CommonGalaxyCluster):
+    meta: None = Field(default=None, exclude=True)  # type: ignore
+    tag_id: None = Field(default=None, exclude=True)  # type: ignore
+    local: None = Field(default=None, exclude=True)  # type: ignore
+    relationship_type: None = Field(default=None, exclude=True)  # type: ignore
+
     GalaxyElement: list[ExportGalaxyGalaxyElement]
+    Galaxy: GetAllSearchGalaxiesAttributes | None = None
     GalaxyClusterRelation: list = []
     RelationshipInbound: list = []
     Org: GetOrganisationResponse | None = None
