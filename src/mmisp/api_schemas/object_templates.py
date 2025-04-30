20c0ca34d74361a449562d731bfee2c1e4f5232c
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2025-04-30 12:12:24 +0200

    add ObjectTemplateFileImport to api_schemas

--- src/mmisp/api_schemas/object_templates.py
+++ src/mmisp/api_schemas/object_templates.py
@@ -1,35 +1,9 @@
+from uuid import UUID
+
 from pydantic import BaseModel, ConfigDict, Field
-from pydantic.types import UUID
 
 
-class ObjectTemplatesRequirements(BaseModel):
-    requiredOneOf: list[str] | None = None
-    required: list[str] | None = None
-
-
-class ObjectTemplate(BaseModel):
-    model_config = ConfigDict(populate_by_name=True)
-
-    id: int
-    user_id: int
-    org_id: int
-    uuid: UUID
-    name: str
-    meta_category: str = Field(..., alias="meta-category")
-    description: str
-    version: str
-    requirements: ObjectTemplatesRequirements
-    fixed: bool
-    active: bool
-
-
-class ObjectTemplateElement(BaseModel):
-    model_config = ConfigDict(populate_by_name=True)
-
-    id: int
-    object_template_id: int
-    object_relation: str
-    type: str  # AttributeType?
+class CommonObjectTemplateElement(BaseModel):
     ui_priority: int = Field(..., alias="ui-priority")
     categories: list
     sane_default: list
@@ -37,6 +11,49 @@ class ObjectTemplateElement(BaseModel):
     description: str
     disable_correlation: bool | None
     multiple: bool
+
+
+class ImportObjectTemplateElement(CommonObjectTemplateElement):
+    misp_attribute: str = Field(..., alias="misp-attribute")
+    recommended: bool
+    to_ids: bool
+
+
+class ObjectTemplatesRequirements(BaseModel):
+    requiredOneOf: list[str] | None = None
+    required: list[str] | None = None
+
+
+class ImportObjectTemplate(BaseModel):
+    version: int
+    description: str
+    meta_category: str = Field(..., alias="meta-category")
+    uuid: UUID
+    name: str
+
+
+class ImportObjectTemplateFile(ImportObjectTemplate, ObjectTemplatesRequirements):
+    attributes: dict[str, ImportObjectTemplateElement]
+
+
+class ObjectTemplate(ImportObjectTemplate):
+    model_config = ConfigDict(populate_by_name=True)
+
+    id: int
+    user_id: int
+    org_id: int
+    requirements: ObjectTemplatesRequirements
+    fixed: bool
+    active: bool
+
+
+class ObjectTemplateElement(CommonObjectTemplateElement):
+    model_config = ConfigDict(populate_by_name=True)
+
+    id: int
+    object_template_id: int
+    object_relation: str
+    type: str  # AttributeType?
 
 
 class RespObjectTemplateView(BaseModel):
